5ad91457421c984b41c5312d49a00ce3e36a3fd2
ttapullimaa <ttapullima@xternal.biz>
2021-08-25 22:17:30 -0500

    Fuente Original  MASTER

---
+++ Romsoft.GESTIONCLINICA.DataAccess/Interfaces/ICVN_TARIFARIO_SEGUSRepository/ICVN_TARIFARIO_SEGUSRepository.cs
@@ -0,0 +1,9 @@
+﻿using Romsoft.GESTIONCLINICA.DataAccess.Core;
+
+namespace Romsoft.GESTIONCLINICA.DataAccess.Interfaces.ICVN_TARIFARIO_SEGUSRepository
+{
+    public interface ICVN_TARIFARIO_SEGUSRepository<T> : IRepository<T> where T : class
+    {
+        bool Exists(T entity);
+    }
+}
